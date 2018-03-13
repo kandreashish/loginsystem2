a953e115c8235c7230624f04d635a78bbf58c55d
kandreashish <36927489+kandreashish@users.noreply.github.com>
2018-03-13 11:59:51 +0530

    new

--- src/g/DBIntializer.java
+++ src/g/DBIntializer.java
@@ -1,8 +1,8 @@
 package g;
 
 public interface DBIntializer {
-String DRIVER="com.mysql.jdbc.Driver";
-String CON_STRING="jdbc:mysql://localhost:3306/db";
-String USERNAME="root";
-String PASSWORD="1234";
+String DRIVER="org.postgresql.Driver";
+String CON_STRING="jdbc:postgresql://ec2-54-221-207-184.compute-1.amazonaws.com:5432/dmenjcnj941am";
+String USERNAME="ljfkvyevjutmyd";
+String PASSWORD="670c219f555f814040d978f2a58305a567ff4b5a687a940164cf77cb3867398d";
 }
